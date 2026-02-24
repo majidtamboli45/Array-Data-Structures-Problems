c5a1795e4ce29ceb6000a24b1765bdb60f2125f7
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-02-24 08:46:34 +0530

    Add files via upload

---
+++ 704BinarySearch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+
+    Solution obj;
+    int result = obj.search(nums, target);
+
+    cout << "Index: " << result << endl;
+
+    return 0;
+}
