afbd4accda1c7fe53405dc49511e12ef403a1c2d
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-03-01 00:11:07 +0530

    Add files via upload

---
+++ 724FindPivotindex.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        
+        for(int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if(leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 1, -1};
+
+    Solution obj;
+    int result = obj.pivotIndex(nums);
+
+    cout << "Pivot Index: " << result << endl;
+
+    return 0;
+}
